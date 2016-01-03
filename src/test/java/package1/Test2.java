e39651dc32b9caa4394e6e8367a2667e87ae787b
harsimran dhanoa <harsimran dhanoa>
2016-01-03 14:22:35 +0530

    second commit

---
+++ src/test/java/package1/Test2.java
@@ -0,0 +1,13 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	 public void test2()
+	 
+	    {
+	    	System.out.println("$$**********This is test2***************");
+
+	    }
+}
---
+++ src/test/java/package1/Test3.java
@@ -0,0 +1,18 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Test3 {
+
+	@Test
+	 public void test3()
+	 
+	    {
+	    	System.out.println("$$**********This is test3***************");
+
+	    }
+	
+	
+	
+	
+}
